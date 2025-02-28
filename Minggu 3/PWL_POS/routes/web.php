7ddb87976b8cfa444e94a496d7eaf1a12517ff5d
dhevinaagustina <dhevinaagustina@gmail.com>
2025-02-28 22:57:40 +0700

    Praktikum 4

--- Minggu 3/PWL_POS/routes/web.php
+++ Minggu 3/PWL_POS/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/level', [LevelController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
