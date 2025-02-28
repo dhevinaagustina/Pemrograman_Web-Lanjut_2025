6bb09f71d819d0820fb38c60b7f1f163a24a62e8
dhevinaagustina <dhevinaagustina@gmail.com>
2025-03-01 00:18:52 +0700

    Praktikum 6

--- Minggu 3/PWL_POS/routes/web.php
+++ Minggu 3/PWL_POS/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,7 @@ use App\Http\Controllers\KategoriController;
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
