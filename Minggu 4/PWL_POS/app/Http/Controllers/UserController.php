f6c3ab87b8acfeec38fca1ff5b22e58f31368a2c
dhevinaagustina <dhevinaagustina@gmail.com>
2025-03-06 23:54:17 +0700

    commit Relationships

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,45 +10,10 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        // //coba akses usermodel
-        // $user = UserModel::all(); //ambil semua dari tabel m_user
-        // return view('user', ['data' => $user]);
-
-        //tambah data user dengan eloquent model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-        // $user = UserModel::find(1);
-        // $user = UserModel::where('level_id', 1)->first();
-        // $user = UserModel::firstWhere('level_id', 1);
-        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
-        //     abort(404);
-        // });
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        // $user = UserModel::where('level_id',2)->count();
-        // dd($user);
-        // return view('user', ['data' => $user]);
-
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-
+        $users = UserModel::with('level')->get();
+        return view('user', ['data' => $users]);
     }
+    
 
     public function tambah(){
         return view('user_tambah');
