42e2281b276533aed4c84967ecac3275ff6a4046
dhevinaagustina <dhevinaagustina@gmail.com>
2025-03-22 11:27:31 +0700

    Commit Prak 2

--- Minggu 5 (Revisi)/PWL_POS/routes/web.php
+++ Minggu 5 (Revisi)/PWL_POS/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +17,24 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-Route::get('/user/tambah',[UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
+// Jobsheet 5
+Route::get('/', [WelcomeController::class,'index']);
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+// Route::get('/user/tambah',[UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
+
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
