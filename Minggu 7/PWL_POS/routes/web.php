9c4e44d6347318aaf6dae28eef6459b1382b3acb
dhevinaagustina <dhevinaagustina@gmail.com>
2025-04-03 13:21:50 +0700

    Prak 2

--- Minggu 7/PWL_POS/routes/web.php
+++ Minggu 7/PWL_POS/routes/web.php
@@ -27,10 +27,30 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postLogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::middleware(['auth'])->group(function() { //artinya semua route di dalam group ini harus login dulu
+Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+    Route::get('/', [WelcomeController::class, 'index']);
 
-    
+    // route Level
 
+    // artinya semua route di dalam group ini harus punya role ADM ( Administrator )
+    Route::middleware(['authorize:ADM'])->group(function () {
+
+        Route::get('/level', [LevelController::class, 'index']);
+
+        Route::post('/level/list', [LevelController::class, 'list']); // untuk list json datatables
+
+        Route::get('/level/create', [LevelController::class, 'create']);
+
+        Route::post('/level', [LevelController::class, 'store']);
+
+        Route::get('/level/{id}/edit', [LevelController::class, 'edit']); // untuk tampilkan form edit
+        Route::put('/level/{id}', [LevelController::class, 'update']); // untuk proses update data
+
+        Route::delete('/level/{id}', [LevelController::class, 'destroy']); // untuk proses hapus data
+
+    });
+
+    // route Kategori
 });
     
 
