1b14516075e81bd9861aa7564409c2f045c81646
dhevinaagustina <dhevinaagustina@gmail.com>
2025-04-07 20:40:54 +0700

    Tuguas 3 no 1 (user)

--- Minggu 8/PWL_POS/routes/web.php
+++ Minggu 8/PWL_POS/routes/web.php
@@ -54,6 +54,7 @@ Route::middleware(['authorize:ADM,MNG,STF'])->prefix('user')->group(function (){
     Route::get('/import', [UserController::class, 'import']);
     Route::post('/import_ajax', [UserController::class, 'import_ajax']);
     Route::get('/export_excel', [UserController::class, 'export_excel']);
+    Route::get('/export_pdf', [UserController::class, 'export_pdf']);
 });
 
 Route::middleware(['authorize:ADM,MNG,STF'])->prefix('level')->group(function () {
