d0ecebb0ffd7bbdbc8ff0dd73198fad4d5006993
dhevinaagustina <dhevinaagustina@gmail.com>
2025-04-07 18:50:17 +0700

    Tugas 2 no 1 (user)

--- Minggu 8/PWL_POS/routes/web.php
+++ Minggu 8/PWL_POS/routes/web.php
@@ -53,6 +53,7 @@ Route::middleware(['authorize:ADM,MNG,STF'])->prefix('user')->group(function (){
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
     Route::get('/import', [UserController::class, 'import']);
     Route::post('/import_ajax', [UserController::class, 'import_ajax']);
+    Route::get('/export_excel', [UserController::class, 'export_excel']);
 });
 
 Route::middleware(['authorize:ADM,MNG,STF'])->prefix('level')->group(function () {
